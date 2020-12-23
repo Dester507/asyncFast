3cf87f1deee8fd364117e2016234e9cdcc216fa5
Anatolii <tolikdemchuk507@gmail.com>
2020-12-23 13:40:17 +0200

    Well

--- app.py
+++ app.py
@@ -1,31 +1,53 @@
-import asyncio
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
-from models import Base, User
+from models import Base, User, Taxes
+from config import HOST, PORT, DATABASE, USERNAME, PASSWORD
 
 app = FastAPI()
 
 
 class UserAPI(BaseModel):
+    username: str
     name: str
     surname: str
     age: int
 
 
-async def async_add_user(name=None, surname=None, age=None):
-    engine = create_async_engine("sqlite:///data.db", echo=True)
+class TaxesAPI(BaseModel):
+    username: str
+    water: float
+    light: float
+    gaz: float
+
+
+async def async_add_user():
+    engine = create_async_engine(f"postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}", echo=True)
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    async with AsyncSession(engine) as session:
-        async with session.begin():
-            session.add(User(name, surname,  age))
-        await session.commit()
+    return {"engine": engine}
 
 
 @app.post("/add")
-async def add_user(user: UserAPI):
-    await async_add_user(user.name, user.surname, user.age)
+async def add_user(user: UserAPI, taxes: TaxesAPI, eng: dict = Depends(async_add_user)):
+    async with AsyncSession(eng['engine']) as session:
+        async with session.begin():
+            session.add(User(user.username, user.name, user.surname, user.age))
+            session.add(Taxes(taxes.username, taxes.water, taxes.light, taxes.gaz))
+        await session.commit()
+    return {"Result": "Good"}
+
+
+@app.get("/remove/{username}")
+async def remove_user(username: str, eng: dict = Depends(async_add_user)):
+    async with AsyncSession(eng['engine']) as session:
+        async with session.begin():
+            user = session.query(User).filter(User.username == username).first()
+            if user:
+                session.query(Taxes).filter(Taxes.username == user.username).first().delete()
+                session.delete(user)
+            else:
+                return {"Error": "User with this username does not exists"}
+        await session.commit()
     return {"Result": "Good"}
--- models.py
+++ models.py
@@ -1,18 +1,36 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, ForeignKey, Float
 
 Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "users_api"
 
     id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
     name = Column(String, nullable=False)
     surname = Column(String, nullable=False)
     age = Column(Integer, nullable=False)
 
-    def __init__(self, name, surname, age):
+    def __init__(self, username, name, surname, age):
+        self.username = username
         self.name = name
         self.surname = surname
         self.age = age
+
+
+class Taxes(Base):
+    __tablename__ = 'taxes_api'
+
+    id = Column(Integer, primary_key=True)
+    person = Column(String, unique=True)
+    water = Column(Float, nullable=False)
+    light = Column(Float, nullable=False)
+    gaz = Column(Float, nullable=False)
+
+    def __init__(self, person, water, light, gaz):
+        self.person = person
+        self.water = water
+        self.light = light
+        self.gaz = gaz
