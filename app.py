c4841d2e167ebfdd29f015cc782e205dfdc8f33b
Anatolii <tolikdemchuk507@gmail.com>
2020-12-24 11:58:54 +0200

    Well

--- app.py
+++ app.py
@@ -30,12 +30,17 @@ async def async_add_user():
 
 
 async def delete_info(session, username):
-    user = session.query(User).filter(User.username == username).first()
+    user = await session.query(User).filter(User.username == username).first()
     if user:
-        session.query(Taxes).filter(Taxes.username == username).first().delete()
+        await session.query(Taxes).filter(Taxes.username == username).first().delete()
+        await session.delete(user)
         return True
     else:
         return False
+
+
+async def get_all(session):
+    return await session.query(User).all()
 
 
 @app.post("/add")
@@ -48,12 +53,27 @@ async def add_user(user: UserAPI, taxes: TaxesAPI, eng: dict = Depends(async_add
     return {"Result": "Good"}
 
 
-@app.post("/delete{username}")
+@app.post("/delete/{username}")
 async def remove_user(username: str, eng: dict = Depends(async_add_user)):
     async with AsyncSession(eng['engine']) as session:
         async with session.begin():
             status = await session.run_sync(delete_info, username)
-            if status is False:
-                return {"Error": "User with this username does not exists"}
         await session.commit()
-    return {"Result": "Good"}
+    if status is False:
+        return {"Error": "User with this username does not exists"}
+    else:
+        return {"Result": "Good"}
+
+
+@app.get("/users")
+async def get_all(eng: dict = Depends(async_add_user)):
+    async with AsyncSession(eng['engine']) as session:
+        async with session.begin():
+            users = await session.run_sync(get_all)
+    result = {}
+    counter = 0
+    for user in users:
+        counter += 1
+        result[f"User #{counter}"] = {"username": user.username, "name": user.name, "surname": user.surname,
+                                      "age": user.age}
+    return result
